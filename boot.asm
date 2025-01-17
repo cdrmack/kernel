b7889d051cc27fd8201b4d1a3541232546349656
cdrmack <git.pynvt@passmail.net>
2025-01-17 10:21:33 +0100

    add comments

--- boot.asm
+++ boot.asm
@@ -2,16 +2,16 @@
 ;;; ndisasm boot.bin
 ;;; qemu-system-x86_64 -hda boot.bin
 
-    ORG 0x7c00
-    BITS 16
+    ORG 0x7C00                  ; this is where BIOS copies bootloader to the RAM
+    BITS 16                     ; real mode is 16 bit, this is the initial operating mode
 
 start:
-    mov ah, 0eh
+    mov ah, 0xE                 ; display char
     mov al, 'W'
-    mov bx, 0
-    int 0x10
+    int 0x10                    ; call BIOS interrupt
 
-    jmp $
+hang:
+    jmp hang                    ; infinite loop to avoid crash
 
-    times 510-($ - $$) db 0
-    dw 0xAA55
+    times 510 - ($ - $$) db 0   ; boot sector is always 512 bytes, fill the rest with 0s
+    dw 0xAA55                   ; boot sector signature
