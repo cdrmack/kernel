e571e5702431cabea4341dd464fd0b8b4c6be4b2
cdrmack <git.pynvt@passmail.net>
2025-01-22 09:48:04 +0100

    add custom interrupt 0

--- boot.asm
+++ boot.asm
@@ -10,15 +10,34 @@ _start:
 start:
     jmp 0x7C0:step2             ; set code segment to 0x7C0 and jump to step2 label
 
+handle_zero:                    ; custom interrupt 0 (divide by 0)
+    mov ah, 0xE                 ; display char
+    mov al, '0'
+    mov bx, 0x0
+    int 0x10                    ; call BIOS interrupt
+    iret                        ; return from the interrupt
+
 step2:
     cli                         ; clear interrupts
     mov ax, 0x7C0               ; 0x7C00 is where BIOS should copy the bootloader to the RAM
     mov ds, ax                  ; data segment
     mov es, ax                  ; extra segment
-    mov ax, 0x00
+    mov ax, 0x0
     mov ss, ax                  ; stack segment
     mov sp, 0x7C00              ; stack pointer (it grows downwards)
     sti                         ; enable interrupts
+
+    cli
+    mov word[ss:0x00], handle_zero   ; override first byte (interrupt zero) in the interrupts vector
+                                     ; we use ss which is 0x0 because [0x0] (without segment specified) would use ds by default which is 0x7C0
+    mov ax, cs                  ; store code segment
+    mov word[ss:0x02], ax       ; set segment of the interrupt handler
+    sti
+
+    mov ax, 0x0
+    div ax                      ; divide by 0, will trigger interrupt 0
+    ;; int 0                            ; call interrupt zero directly
+
     mov si, string              ; store address of the string inside si
 
 print_loop:
