c92d4c073e7f5e26b8a50d787e6d30cad4954366
cdrmack <git.pynvt@passmail.net>
2025-01-23 10:08:04 +0100

    add Makefile, read sector from the disk

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Minimalistic bootloader for x86-64.
 
 ```shell
-nasm -f bin boot.asm -o boot.bin
+gmake
 qemu-system-x86_64 -drive file=boot.bin,format=raw
 ```
 
--- boot.asm
+++ boot.asm
@@ -10,20 +10,6 @@ _start:
 start:
     jmp 0x7C0:step2             ; set code segment to 0x7C0 and jump to step2 label
 
-handle_zero:                    ; custom interrupt 0 (divide by 0)
-    mov ah, 0xE                 ; display char
-    mov al, '0'
-    mov bx, 0x0
-    int 0x10                    ; call BIOS interrupt
-    iret                        ; return from the interrupt
-
-handle_one:                     ; custom interrupt 1
-    mov ah, 0xE
-    mov al, '1'
-    mov bx, 0x0
-    int 0x10
-    iret
-
 step2:
     cli                         ; clear interrupts
     mov ax, 0x7C0               ; 0x7C00 is where BIOS should copy the bootloader to the RAM
@@ -34,40 +20,47 @@ step2:
     mov sp, 0x7C00              ; stack pointer (it grows downwards)
     sti                         ; enable interrupts
 
-    cli
-    mov word[ss:0x00], handle_zero   ; override first byte (interrupt zero) in the interrupts vector table
-                                     ; we use ss which is 0x0 because [0x0] (without segment specified) would use ds by default which is 0x7C0
-    mov ax, cs                  ; store code segment
-    mov word[ss:0x02], ax       ; set segment of the interrupt handler
+    ;; https://www.ctyme.com/intr/rb-0607.htm
+    mov ah, 0x2                 ; read sector command
+    mov al, 0x1                 ; how many sectors
+    mov ch, 0x0                 ; low eight bits of cylinder number
+    mov cl, 0x2                 ; sector number
+    mov dh, 0x0                 ; head number
+    mov bx, buffer              ; data buffer
+    int 0x13                    ; interrupt for reading disk sector(s) into memory
 
-    mov word[ss:0x04], handle_one
-    mov word[ss:0x06], ax
-    sti
+    jc error                    ; jump if CF is set
+    mov si, buffer
+    call print
+    jmp $
 
-    ;; mov ax, 0x0
-    ;; div ax                      ; divide by 0, will trigger interrupt 0
-    ;; int 0                            ; call interrupt zero directly
-    int 1
+error:
+    mov si, error_msg
+    call print
+    jmp $
 
-    mov si, string              ; store address of the string inside si
+print:
+    mov bx, 0
 
-print_loop:
+.loop:
     lodsb                       ; load byte at DS:SI (data segment, si register is the offset) into al and increment si
                                 ; DS * 16 + SI = 0x7C0 * 16 + SI = 0x7C00 + SI
 
     cmp al, 0                   ; check if end of string
-    je done
+    je .done
 
     mov ah, 0xE                 ; display char
     int 0x10                    ; call BIOS interrupt
 
-    jmp print_loop
+    jmp .loop
 
-done:
-hang:
-    jmp hang                    ; infinite loop to avoid crash
+.done:
+    jmp $                       ; infinite loop to avoid crash
 
-    string db 'hello from the bootloader!', 0
+error_msg:
+    db 'Failed to load sector', 0
 
     times 510 - ($ - $$) db 0   ; boot sector is always 512 bytes, fill the rest with 0s
     dw 0xAA55                   ; boot sector signature
+
+buffer:
